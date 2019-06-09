923724e69e6b3c0ebe5ce65b8bb9c02f41a15aeb
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-06-09 23:37:33 +0900

    Make sponsors page

---
+++ pages/Sponsors.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  section#sponsors
+    .wrapper
+      p.text.sponsors_text <span class='text-emphasis'>【主催】</span>
+        <br/>みやぎプロコン実行委員会
+
+      p.text.sponsors_text <span class='text-emphasis'>【共催】</span>
+        <br/>仙台高等専門学校
+        <br/>PCN仙台
+
+      p.text.sponsors_text <span class='text-emphasis'>【協力】</span>
+        <br/>一般社団法人PCN
+        <br/>一般社団法人イトナブ石巻
+
+      p.text.sponsors_text <span class='text-emphasis'>【後援(予定)】</span>
+        <br/>宮城県
+        <br/>仙台市
+        <br/>宮城県教育委員会
+        <br/>仙台市教育委員会
+        <br/>宮城県高度情報化推進協議会
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+#sponsors
+  width 100%
+  margin-bottom 40vmin
+
+.sponsors_text
+  font-size 1.4rem
+  text-align center
+  margin-bottom 10vmin
+</style>
