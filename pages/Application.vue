cabb8573a44ff64aacd6c9d8d1ab82b26f6f48f8
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-06-09 23:27:19 +0900

    Update the schedule page

--- pages/Application.vue
+++ pages/Application.vue
@@ -1,30 +1,30 @@
 <template lang="pug">
   section#application
-    .application_wrapper
-      h1.application_title 応募概要
+    .wrapper
+      h1.heading 応募概要
       .application_description
-        p.application_text 第1回みやぎプロコンでは、宮城県内の小中学生が作ったプログラム作品を募集します。
+        p.text 第1回みやぎプロコンでは、宮城県内の小中学生が作ったプログラム作品を募集します。
           <br/>プログラミングに触れたことがない初心者も大歓迎です。
           <br/>各地で実施する関連ワークショップに参加して、挑戦してみよう。
           <br/>
           <br/>最終審査会＆表彰式は、10月27日(日)仙台高等専門学校広瀬キャンパスで開催！
-          <br/><span class='application_text-emphasis'>賞品には、ノートパソコンやタブレットなど豪華な副賞も多数！</span>
+          <br/><span class='text-emphasis'>賞品には、ノートパソコンやタブレットなど豪華な副賞も多数！</span>
 
-      h2.application_subheading 募集内容
-      p.application_text テーマ：自由
+      h2.subheading 募集内容
+      p.text テーマ：自由
         <br/>オリジナルのブログラム作品であれば、ジャンルや言語、作成ツールは問いません。
         <br/>・ゲームやアプリなどのソフトウェア
         <br/>・ロボットや電子工作などのハードウェア
         <br/>・PCやスマートフォン、タブレットで動作するwebプログラム 等
         <br/>自由な発想と組合せで作品をつくって気軽に応募しよう！
 
-      h2.application_subheading 応募資格
-      p.application_text 宮城県の小学生・中学生(隣県も可)
+      h2.subheading 応募資格
+      p.text 宮城県の小学生・中学生(隣県も可)
         <br/>・2人以上のチームでつくった作品でも応募可
         <br/>・1人（1チーム）何作品でも応募可（※但しノミネートは1作品のみ）
 
-      h2.application_subheading 応募方法
-      p.application_text 応募方法の詳細は、後日お知らせいたします
+      h2.subheading 応募方法
+      p.text 応募方法の詳細は、後日お知らせいたします
 </template>
 
 <script>
@@ -36,42 +36,8 @@ export default {}
   width 100%
   margin-bottom 40vmin
 
-.application_wrapper
-  width 80%
-  max-width 700px
-  display flex
-  flex-direction column
-  justify-content center
-  align-items center
-  margin 0 auto
-
-.application_title
-  // width 100%
-  font-size 2rem
-  letter-spacing 0.1rem
-  font-weight 600
-  margin-bottom 2rem
-  background: linear-gradient(transparent 70%, THEME_ORANGE 30%);
-
 .application_description
   background-color THEME_GRAY_T
   padding 3rem 3rem
   border-radius 3rem
-
-.application_subheading
-  width 100%
-  font-size 1.6rem
-  line-height 2.5rem
-  font-weight 400
-  margin-bottom 3vmin
-  background: linear-gradient(transparent 80%, THEME_BLUE 20%);
-  margin-top 10vmin
-
-.application_text
-  width 100%
-  font-size 1.2rem
-  line-height 2.5rem
-  &-emphasis
-    font-weight 600
-    background: linear-gradient(transparent 80%, THEME_ORANGE 20%);
 </style>
--- pages/Schedule.vue
+++ pages/Schedule.vue
@@ -1,8 +1,25 @@
 <template lang="pug">
   section#schedule
-    .schedule_wrapper
-      h1.schedule_title スケジュール
-      p.schedule_text comming soon...
+    .wrapper
+      h1.heading スケジュール
+
+      h2.subheading 2019年7月～
+      p.text 関連ワークショップを県内各地で実施
+        <br/>※実施スケジュール等は、随時発表いたします
+
+      h2.subheading 2019年8月1日(木)～9月30日(月)まで
+      p.text 作品応募期間
+
+      h2.subheading 2019年10月上旬
+      p.text 一次審査追加の通知
+
+      h2.subheading 2019年10月27日
+      p.text <span class='text-emphasis'>最終審査会＆表彰式</span>
+        <br/>場所：仙台高等専門学校広瀬キャンパス
+        <br/>(高専祭と同時開催！)
+        <br/>ノミネート作品の展示や子供たちによる作品のプレゼン、
+        <br/>ゲストトークセッション、ワークショップなど楽しい催しも実施予定。
+        <br/>詳細は随時発表します。
 </template>
 
 <script>
@@ -13,24 +30,4 @@ export default {}
 #schedule
   width 100%
   margin-bottom 40vmin
-
-.schedule_wrapper
-  width 80%
-  max-width 740px
-  display flex
-  flex-direction column
-  justify-content center
-  align-items center
-  margin 0 auto
-
-.schedule_title
-  font-size 2rem
-  letter-spacing 0.1rem
-  font-weight 600
-  margin-bottom 2rem
-  background: linear-gradient(transparent 70%, THEME_ORANGE 30%);
-
-.schedule_text
-  font-size 1.3rem
-  line-height 2.5rem
 </style>
