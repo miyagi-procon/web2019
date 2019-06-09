02fde77b4a3c1bf37e2d91b76c1c365fc2d675bb
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-06-09 22:34:42 +0900

    Update sentenses in application

--- pages/Application.vue
+++ pages/Application.vue
@@ -7,19 +7,22 @@
         <br/>各地で実施する関連ワークショップに参加して、挑戦してみよう。
       p.application_text 最終審査会＆表彰式は、10月27日(日)仙台高等専門学校広瀬キャンパスで開催！
         <br/>賞品には、ノートパソコンやタブレットなど豪華な副賞も多数！
-      p.application_text ●募集内容
-        <br/>テーマ：自由
+
+      h2.application_subheading 募集内容
+      p.application_text テーマ：自由
         <br/>オリジナルのブログラム作品であれば、ジャンルや言語、作成ツールは問いません。
         <br/>・ゲームやアプリなどのソフトウェア
         <br/>・ロボットや電子工作などのハードウェア
         <br/>・PCやスマートフォン、タブレットで動作するwebプログラム 等
         <br/>自由な発想と組合せで作品をつくって気軽に応募しよう！
-      p.application_text ●応募資格
-        <br/>宮城県の小学生・中学生(隣県も可)
+
+      h2.application_subheading 応募資格
+      p.application_text 宮城県の小学生・中学生(隣県も可)
         <br/>・2人以上のチームでつくった作品でも応募可
         <br/>・1人（1チーム）何作品でも応募可（※但しノミネートは1作品のみ）
-      p.application_text ●応募方法
-        <br/>応募方法の詳細は、後日お知らせいたします
+
+      h2.application_subheading 応募方法
+      p.application_text 応募方法の詳細は、後日お知らせいたします
 </template>
 
 <script>
@@ -41,11 +44,20 @@ export default {}
   margin 0 auto
 
 .application_title
+  // width 100%
   font-size 2rem
   letter-spacing 0.1rem
   font-weight 600
   margin-bottom 2rem
-  background: linear-gradient(transparent 70%, THEME_BLUE 30%);
+  background: linear-gradient(transparent 70%, THEME_ORANGE 30%);
+
+.application_subheading
+  width 100%
+  font-size 1.6rem
+  line-height 2.5rem
+  font-weight 400
+  margin-bottom 3vmin
+  background: linear-gradient(transparent 80%, THEME_BLUE 20%);
 
 .application_text
   width 100%
