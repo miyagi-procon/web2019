5869c4bcbc3143035d74aad8e0478bcd9cb14a2e
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-06-22 21:47:27 +0900

    Add link to story page in the top image

--- pages/top/Top.vue
+++ pages/top/Top.vue
@@ -4,6 +4,7 @@
     top-image2
     top-image3
     top-image4
+    router-link.top_stories(to="/stories") コンセプトストーリーへ
 </template>
 
 <script>
@@ -45,4 +46,16 @@ export default {
   @media screen and (min-width: 740px)
     max-height 100vh
     height calc(100vw * 2 / 3)
+
+.top_stories
+  position absolute
+  right 0
+  bottom 2%
+  background-color BG_COLOR_T
+  font-size 1rem
+  padding 1rem
+  text-decoration none
+  border-radius 1rem 0 0 1rem
+  &:visited
+    color BASE_COLOR
 </style>
