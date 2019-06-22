3d1968892f4537b9e2130536867789dec507ca08
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-06-22 17:50:39 +0900

    Make a component of TopImage1

---
+++ components/TopImage1.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .top_wrapper.top_wrapper1
+    p.top_text 気分にあわせて<br/>光る花。
+    img.top_image(src="~assets/images/slideillust1.png")
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+.top_wrapper1
+  background-color THEME_ORANGE
+
+.top_text
+  font-weight 700
+  color BG_COLOR
+  text-align center
+  position absolute
+  @media screen and (max-width: 740px)
+    font-size 2rem
+    left 10vw
+    top 20%
+  @media screen and (min-width: 740px)
+    font-size 4rem
+    left 10vw
+    top 50%
+
+.top_image
+  position absolute
+  @media screen and (max-width: 740px)
+    max-height 70vw
+    right 10%
+    bottom 10%
+  @media screen and (min-width: 740px)
+    max-height 60vmin
+    right 10%
+    bottom 10%
+</style>
--- pages/top/Top.vue
+++ pages/top/Top.vue
@@ -1,13 +1,20 @@
 <template lang="pug">
   section#top
-    .title
-      .top_right
-      .top_left
-      img#logo(src="~assets/images/logo.png")
+    top-image1
+    //- .title
+    //-   .top_right
+    //-   .top_left
+    //-   img#logo(src="~assets/images/logo.png")
 </template>
 
 <script>
-export default {}
+import TopImage1 from '~/components/TopImage1.vue'
+
+export default {
+  components: {
+    TopImage1
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
@@ -15,41 +22,12 @@ export default {}
   width 100%
   margin-bottom 40vmin
 
-.title
-  background-image url('~assets/images/top1.jpg')
-  background-size cover
+.top_wrapper
   width 100%
-  max-height 100vh
-  height calc(100vw * 2 / 3)
   position relative
-  display flex
-  align-items center
-  justify-content center
-
-#logo
-  width 40vw
-  height 40vw
-  max-width calc(1080px * 0.4)
-  max-height calc(1080px * 0.4)
-  margin-top 0
-
-.top_right
-  position absolute
-  background-color THEME_ORANGE
-  width 20vw
-  height 20vw
-  max-width calc(1080px * 0.2)
-  max-height calc(1080px * 0.2)
-  bottom 0
-  right 0
-
-.top_left
-  position absolute
-  background-color THEME_BLUE
-  width 20vw
-  height 20vw
-  max-width calc(1080px * 0.2)
-  max-height calc(1080px * 0.2)
-  top 0
-  left 0
+  @media screen and (max-width: 740px)
+    height 100vw
+  @media screen and (min-width: 740px)
+    max-height 100vh
+    height calc(100vw * 2 / 3)
 </style>
