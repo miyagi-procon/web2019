77dba79d956480ebdd1269c2978082b63e0fbf99
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-06-26 21:55:07 +0900

    Add new images of sponsors

--- pages/top/Sponsors.vue
+++ pages/top/Sponsors.vue
@@ -11,9 +11,10 @@
         p.text.sponsors_text.sponsors_bottom <span class='text-emphasis'>【主催】</span>
           <br/>みやぎプロコン実行委員会
 
-        p.text.sponsors_text.sponsors_bottom <span class='text-emphasis'>【共催】</span>
-          <br/>仙台高等専門学校
-          <br/>PCN仙台
+        p.text.sponsors_text <span class='text-emphasis'>【共催】</span>
+          .sponsors_image.sponsors_bottom
+            img.sponsors_image_item2(src="~assets/images/pcn.jpg")
+            img.sponsors_image_item(src="~assets/images/sendai-kosen.jpg")
 
         p.text.sponsors_text.sponsors_bottom <span class='text-emphasis'>【後援】</span>
           <br/>宮城県
@@ -21,6 +22,11 @@
           <br/>宮城県教育委員会
           <br/>仙台市教育委員会
           <br/>宮城県高度情報化推進協議会
+
+        p.text.sponsors_text <span class='text-emphasis'>【特別協力】</span>
+          .sponsors_image.sponsors_bottom
+            img.sponsors_image_item3(src="~assets/images/thinkShake.png")
+            img.sponsors_image_item4(src="~assets/images/folk-glocal-works.png")
 
         p.text.sponsors_text.sponsors_bottom <span class='text-emphasis'>【協力】</span>
           <br/>一般社団法人PCN
@@ -63,5 +69,26 @@ export default {}
     width 80%
   @media screen and (min-width: 740px)
     width 60%
+  margin-bottom 5vmin
+
+.sponsors_image_item2
+  @media screen and (max-width: 740px)
+    width 80%
+  @media screen and (min-width: 740px)
+    width 60%
+  margin 5vmin 0
+
+.sponsors_image_item3
+  @media screen and (max-width: 740px)
+    width 30%
+  @media screen and (min-width: 740px)
+    width 20%
+  margin 5vmin 0 8vmin
+
+.sponsors_image_item4
+  @media screen and (max-width: 740px)
+    width 40%
+  @media screen and (min-width: 740px)
+    width 30%
   margin-bottom 5vmin
 </style>
