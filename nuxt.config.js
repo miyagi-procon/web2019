5575be3ea9ae6caa47038a97a16f9477cb4dffd3
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-07-04 21:28:29 +0900

    Set meta tags and ogp tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,10 +12,38 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '「みやぎプロコン」は、宮城県のこどもたちにものづくりの楽しさや面白さを体験してほしい！という想いから始まったプログラミングコンテストです。'
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'みやぎプロコン'
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://miyagi-procon.jp/'
+      },
+      { hid: 'og:title', property: 'og:title', content: 'みやぎプロコン' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '「みやぎプロコン」は、宮城県のこどもたちにものづくりの楽しさや面白さを体験してほしい！という想いから始まったプログラミングコンテストです。'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://miyagi-procon.jp/ogp.png'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       // { rel: 'stylesheet', href: 'href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"' }
     ]
   },
@@ -28,24 +56,25 @@ export default {
   /*
    ** Global CSS
    */
-  css: [
-    '~assets/stylus/base.styl'
-  ],
+  css: ['~assets/stylus/base.styl'],
 
   /*
    ** Plugins to load before mounting the App
    */
   plugins: [
     // { src: '~plugins/vue-scrollto' },
-    { src: '~plugins/windowState', ssr: false },
+    { src: '~plugins/windowState', ssr: false }
   ],
 
   /*
    ** Nuxt.js modules
    */
   modules: [
-    ['nuxt-stylus-resources-loader', resolve(__dirname, 'assets/stylus/**/*.styl')],
-    'nuxt-fontawesome',
+    [
+      'nuxt-stylus-resources-loader',
+      resolve(__dirname, 'assets/stylus/**/*.styl')
+    ],
+    'nuxt-fontawesome'
   ],
   fontawesome: {
     imports: [
