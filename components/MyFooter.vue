e5ea0e2620321ad15346e09f7aedebb1e95f952e
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-07-16 21:22:44 +0900

    Add an inquiry form to footer

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
   footer#footer
-    p Copyright(c) {{ year }} Miyagi Procon. All Rights Reserved.
+    p.footer_head みやぎプロコン実行委員会
+    .inquiry
+      p [ お問合せ ]
+      p 事務局：<a href="mailto:support@miyagi-procon.jp" class="inquiry_mail">support@miyagi-procon.jp</a>
+      p.contactForm <a href="https://forms.gle/ph6SG63vf1PxzzWb8" class="contactForm_text" target="_blank">お問合せフォームはこちら</a>
+    p.copyright Copyright(c) {{ year }} Miyagi Procon. All Rights Reserved.
 </template>
 
 <script>
@@ -21,9 +26,35 @@ export default {
   text-align center
   flex-direction column
   align-items center
-  height 30px
+  // height 30px
 
-p
+.footer_head
+  font-size 1.2rem
+
+.inquiry
+  width 100%
+  display flex
+  justify-content center
+  text-align center
+  flex-direction column
+  align-items center
+  margin .5rem auto 1rem
+  height 5rem
+  &_mail
+    text-decoration none
+    color BG_COLOR
+
+.copyright
   font-size 0.7rem
   width 85%
+
+.contactForm
+  width 13rem
+  background-color BG_COLOR
+  border-radius 2px
+  margin-top .5rem
+  &_text
+    text-decoration none
+    color BASE_COLOR
+    font-size 0.8rem
 </style>
