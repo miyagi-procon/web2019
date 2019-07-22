55b53f0fcd625ca889097ca9b8618ddd36963b10
Mikkiyee <51771805+Mikkiyee@users.noreply.github.com>
2019-07-22 13:27:50 +0900

    Update index.vue

--- pages/workshop/index.vue
+++ pages/workshop/index.vue
@@ -22,11 +22,22 @@
             day='土'
             date2='7/28'
             day2='日'
+            title='IchigoJamでゲームプログラミング'
+            subtitle='IchigoJamを使って川下りゲームをプログラミングで作ってみよう'
+            place='名取市'
+            organizer='仙台高等専門学校'
+            color='orange'
+            link='https://connpass.com/event/140198/')            
+          workshop-card(
+            date='7/27'
+            day='土'
+            date2='7/28'
+            day2='日'
             title='アイディアハッカソン＆電子工作'
             subtitle='家族の困ったを解決するひみつ道具を作ろう！'
             place='仙台市（長町）'
             organizer='株式会社アイティプロジェクト'
-            color='orange'
+            color='bule'
             link='http://sendai.pcn.club/ws/miyagiprocon2019.html')
           workshop-card(
             date='8/3'
@@ -35,8 +46,19 @@
             subtitle='いろんなセンサーを使ったものづくり体験'
             place='仙台市（長町）'
             organizer='株式会社アイティプロジェクト'
-            color='blue'
+            color='gray'
             link='http://sendai.pcn.club/ws/miyagiprocon2019.html')
+          workshop-card(
+            date='8/3'
+            day='土'
+            date2='8/4'
+            day2='日'
+            title='センサープログラミングで自動ブレーキシステム'
+            subtitle='センサーがついたロボットカーをプログラミングで自由自在に動かそう'
+            place='仙台市'
+            organizer='仙台高等専門学校'
+            color='orange'
+            link='https://connpass.com/event/140190/')            
           workshop-card(
             date='8/5'
             day='月'
@@ -46,7 +68,7 @@
             subtitle='映画スターウォーズに登場するR2ユニットの組立て＆プログラミング'
             place='仙台市（長町）'
             organizer='マジカルコンパススクール'
-            color='gray'
+            color='blue'
             link='http://www.magicalcompath.jp/news/201907l_droid_ws.html')
           workshop-card(
             date='8/7'
@@ -55,7 +77,7 @@
             subtitle='温度センサーで室温調整に挑戦！'
             place='山元町「ICHIGO WORLD」'
             organizer='株式会社GRA'
-            color='orange'
+            color='gray'
             link='https://forms.gle/diFQfkfhhwVTmtRZ7')
           workshop-card(
             date='8/7'
@@ -64,7 +86,7 @@
             subtitle='映画スターウォーズに登場するR2ユニットの組立て＆プログラミング'
             place='仙台市'
             organizer='マジカルコンパススクール'
-            color='blue'
+            color='orange'
             link='http://www.magicalcompath.jp/news/201907l_droid_ws.html')
           workshop-card(
             date='8/9'
@@ -73,7 +95,7 @@
             subtitle='センサー、モーター、サウンド、ライトで何ができるかな？'
             place='富谷市'
             organizer='NPO法人 ふうどばんく東北AGAIN'
-            color='gray'
+            color='blue'
             link='https://www.fut-messe.com/switch-tomiya/')
           workshop-card(
             date='8/11'
@@ -84,7 +106,7 @@
             subtitle='オリジナルゲーム制作やロボット制御に挑戦！'
             place='仙台市'
             organizer='緑と風のガーデン'
-            color='orange'
+            color='gray'
             link='https://midori-kaze-garden.jp/event_201907/')
           workshop-card(
             date='8/17'
@@ -93,7 +115,7 @@
             subtitle='自由研究に使えるかも'
             place='仙台市（長町）'
             organizer='WachHackSendai(株式会社diddyworks, thinkShake製作所)'
-            color='blue'
+            color='orange'
             link='https://peraichi.com/landing_pages/view/wackhacksendai')
           workshop-card(
             date='8/21'
@@ -102,7 +124,7 @@
             subtitle='「身近な困りごと」を解決しよう！（※embotをすでにお持ちの小中学生限定）'
             place='仙台市ドコモ東北ビル'
             organizer='NTTドコモ'
-            color='gray'
+            color='blue'
             link='https://forms.gle/DR1iFV9rn3rjinUd9')
           workshop-card(
             date='8/24'
@@ -113,7 +135,7 @@
             subtitle='家族の困ったを解決するひみつ道具を作ろう！'
             place='仙台市（長町）'
             organizer='株式会社アイティプロジェクト'
-            color='orange'
+            color='gray'
             link='http://sendai.pcn.club/ws/miyagiprocon2019.html')
           workshop-card(
             date='8/25'
@@ -122,7 +144,7 @@
             subtitle='Scratchを使ってゲームを作成'
             place='石巻市'
             organizer='一般社団法人イトナブ'
-            color='blue'
+            color='orange'
             link='http://itnav.jp/itnavjuku/')
 </template>
 
