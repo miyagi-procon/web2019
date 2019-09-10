2e6f30204775c068842f86bac6786d886f5869cc
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-09-10 18:29:28 +0900

    Update sponsors

--- pages/top/Sponsors.vue
+++ pages/top/Sponsors.vue
@@ -3,42 +3,53 @@
     .wrapper
       .sponsors_wrapper
         p.text.sponsors_text <span class='text-emphasis'>【協賛】</span>
-        .sponsors_image.sponsors_bottom
-          a.sponsors_image_link(href="https://www.sakura.ad.jp/" target="_blank")
-            img.sponsors_sakura(src="~assets/images/sakura.jpg")
-          a.sponsors_image_link(href="http://it-p.jp/" target="_blank")
-            img.sponsors_itproject(src="~assets/images/itp.jpg")
-          a.sponsors_image_link(href="http://www.umezawa.co.jp/" target="_blank")
-            img.sponsors_umezawa(src="~assets/images/umezawa.gif")
+        .sponsors_bottom.sponsors_wrapper-gold
+          a.sponsors_item-gold(href="https://www.sakura.ad.jp/" target="_blank")
+            img(src="~assets/images/sakura.jpg")
+          a.sponsors_item-gold(href="http://www.umezawa.co.jp/" target="_blank")
+            img(src="~assets/images/umezawa.png")
+          a.sponsors_item-gold(href="https://www.fujitsu.com/jp/" target="_blank")
+            img(src="~assets/images/fujitsu.jpg")
+          a.sponsors_item-gold(href="https://www.treasuredata.co.jp/" target="_blank")
+            img(src="~assets/images/tresure.jpg")
+          a.sponsors_item-gold(href="http://it-p.jp/" target="_blank")
+            img(src="~assets/images/itp.jpg")
+          .sponsors_wrapper-bronze
+            p.sponsors_bronze 株式会社交宣
+            p.sponsors_bronze 株式会社日立製作所 東北支社
+            p.sponsors_bronze 錦ケ丘ヒルサイドモール
 
       .sponsors_wrapper
+        p.text.sponsors_text <span class='text-emphasis'>【後援】</span>
+          .sponsors_wrapper-silver.sponsors_bottom
+            p.sponsors_item-silver 宮城県
+            p.sponsors_item-silver 仙台市
+            p.sponsors_item-silver 宮城県教育委員会
+            p.sponsors_item-silver 仙台市教育委員会
+            p.sponsors_item-silver 宮城県高度情報化推進協議会
+            p.sponsors_item-silver 山元町教育委員会
         p.text.sponsors_text.sponsors_bottom <span class='text-emphasis'>【主催】</span>
           <br/>みやぎプロコン実行委員会
         p.text.sponsors_text <span class='text-emphasis'>【共催】</span>
-          .sponsors_image.sponsors_bottom
-            a.sponsors_image_link(href="https://www.sendai-nct.ac.jp/" target="_blank")
-              img.sponsors_kousen(src="~assets/images/sendai-kosen.jpg")
-            a.sponsors_image_link(href="http://sendai.pcn.club/" target="_blank")
-              img.sponsors_pcn(src="~assets/images/pcn.jpg")
+          .sponsors_wrapper-silver.sponsors_bottom
+            a.sponsors_item-silver(href="https://www.sendai-nct.ac.jp/" target="_blank")
+              img(src="~assets/images/sendai-kosen.jpg")
+            a.sponsors_item-silver(href="http://sendai.pcn.club/" target="_blank")
+              img(src="~assets/images/pcn.jpg")
+        p.text.sponsors_text <span class='text-emphasis'>【協力】</span>
+          .sponsors_wrapper-silver.sponsors_bottom
+            p.sponsors_item-silver 一般社団法人PCN
+            p.sponsors_item-silver
+              a(href="http://itnav.jp/" target="_blank") 一般社団法人イトナブ石巻
+            p.sponsors_item-silver NTTドコモ
+            p.sponsors_item-silver
+             a(href="https://corp.diddyworks.co.jp/" target="_brank") 株式会社diddyworks
         p.text.sponsors_text <span class='text-emphasis'>【特別協力】</span>
-          .sponsors_image.sponsors_bottom
-            a.sponsors_image_link(href="https://thinkshake.net/" target="_blank")
-              img.sponsors_thinkshake(src="~assets/images/thinkShake.png")
-            a.sponsors_image_link(href="https://www.folkgw.com/" target="_blank")
-              img.sponsors_folk(src="~assets/images/folk-glocal-works.png")
-        p.text.sponsors_text.sponsors_bottom <span class='text-emphasis'>【協力】</span>
-          <br/>一般社団法人PCN
-          <br/><a href="http://itnav.jp/" target="_blank">一般社団法人イトナブ石巻</a>
-          <br/>NTTドコモ
-          <br/><a href="https://corp.diddyworks.co.jp/" target="_brank">株式会社diddyworks</a>
-
-        p.text.sponsors_text.sponsors_bottom <span class='text-emphasis'>【後援】</span>
-          <br/>宮城県
-          <br/>仙台市
-          <br/>宮城県教育委員会
-          <br/>仙台市教育委員会
-          <br/>宮城県高度情報化推進協議会
-          <br/>山元町教育委員会
+          .sponsors_wrapper-silver.sponsors_bottom
+            a.sponsors_item-silver(href="https://thinkshake.net/" target="_blank")
+              img(src="~assets/images/thinkShake.png")
+            a.sponsors_item-silver(href="https://www.folkgw.com/" target="_blank")
+              img(src="~assets/images/folk-glocal-works.png")
 </template>
 
 <script>
@@ -59,44 +70,53 @@ export default {}
     width 80vw
   @media screen and (min-width: 740px)
     width 70vw
+  &-gold
+    width 100%
+    display flex
+    flex-direction column
+    align-items center
+    // justify-content center
+  &-silver
+    display flex
+    flex-direction row
+    flex-wrap wrap
+    align-items flex-start
+  &-bronze
+    margin-top 3rem
+    width 100%
+    display flex
+    flex-direction row
+    flex-wrap wrap
+    align-items center
 
 .sponsors_text
   font-size 1.4rem
   text-align center
+  margin-bottom 2rem
 
 .sponsors_bottom
   margin-bottom 10vmin
 
-.sponsors_image
-  display flex
-  flex-direction column
-  align-items center
-
-.sponsors_image_link
+.sponsors_item-gold
   text-align center
+  width 60%
+  max-width 500px
+  >img
+    text-align center
+    margin-bottom 2vmin
 
-.sponsors_sakura
+.sponsors_item-silver
   text-align center
-  @media screen and (max-width: 740px)
-    width 80%
-  @media screen and (min-width: 740px)
-    width 600px
-  margin-bottom 2vmin
+  width 50%
+  margin-bottom 1rem
+  font-size 1.2rem
+  >img
+    text-align center
+    margin-bottom 2vmin
 
-.sponsors_itproject
+.sponsors_bronze
+  width 33%
   text-align center
-  margin-bottom 5vmin
-  @media screen and (max-width: 740px)
-    width 80%
-  @media screen and (min-width: 740px)
-    width 600px
-
-.sponsors_umezawa
-  text-align center
-  @media screen and (max-width: 740px)
-    width 90%
-  @media screen and (min-width: 740px)
-    width 500px
   margin-bottom 2vmin
 
 .sponsors_kousen
